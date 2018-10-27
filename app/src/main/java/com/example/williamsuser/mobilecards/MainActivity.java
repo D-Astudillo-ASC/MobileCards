2b14bf394f37dddb3015a352c9950d7829732292
D-Astudillo-ASC <dma3@williams.edu>
2018-10-27 15:19:14 -0400

    MobileCards files

---
+++ app/src/main/java/com/example/williamsuser/mobilecards/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.williamsuser.mobilecards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.card_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.card_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.card_question).setVisibility(View.INVISIBLE);
+            }
+        });
+
+        findViewById(R.id.card_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.card_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.card_question).setVisibility(View.VISIBLE);
+            }
+        });
+
+    }
+
+
+}
